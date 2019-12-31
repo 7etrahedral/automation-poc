4e7cb9b28e9c8166dd2dd51185aada3fe049f872
genta wibowo <tetrahedral@gmail.com>
2019-12-31 23:26:32 +0700

    take out the POC to individual repo

--- cypress-poc/cypress/support/step_definitions/facebook/facebook_create_pages_steps.js
+++
@@ -1,9 +0,0 @@
-const browser = require('../../../support/page_objects/facebook/facebook_create_pages_page');
-
-given(/^user is at facebook create page$/, () => {
-  browser.commands.navigateToFacebookCreatePage();
-});
-
-then(/^user will see facebook halaman page$/, () => {
-  browser.commands.assertPageTitleFacebookHalaman();
-});
--- cypress-poc/cypress/support/step_definitions/facebook/facebook_user_steps.js
+++
@@ -1,5 +0,0 @@
-const browser = require('../../../support/page_objects/facebook/facebook_user_page');
-
-then(/^user will see user page$/, () => {
-  browser.commands.assertUserPage();
-});
